913a6ed06a9657d551d36266cd78af4a933f3181
Hardik Savani <itsolutionstuff@gmail.com>
2019-07-20 09:44:28 +0530

    added tutorial setup

---
+++ database/seeds/PermissionTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = [
+           'role-list',
+           'role-create',
+           'role-edit',
+           'role-delete',
+           'product-list',
+           'product-create',
+           'product-edit',
+           'product-delete'
+        ];
+
+
+        foreach ($permissions as $permission) {
+             Permission::create(['name' => $permission]);
+        }
+    }
+}
